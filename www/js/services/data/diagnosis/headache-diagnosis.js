440941fffebaaa80f93de3578c738d41d012e45b
unknown <ThanhVM1@201ThanhVM1.fsoft.fpt.vn>
2014-07-18 11:48:07 +0700

    Update Question

--- www/js/services/data/diagnosis/headache-diagnosis.js
+++ www/js/services/data/diagnosis/headache-diagnosis.js
@@ -1,27 +1,37 @@
 App.factory('HeadacheDiagnosis', function (DiagnosisBuilder) {
 
   var questions = {
+
     'Q001': {
-      type: 'NUMBER',
-      text: 'How old are you?',
+      type: 'GENDER'
+    },
+    'Q002': {
+      type: 'PREGNANT'
+    },
+    'Q003': {
+      type: 'AGE',
       answer: {
         min: 0,
         max: 120
       }
     },
-    'Q002': {
-      type: 'YES_NO',
-      text: 'Are you a boy?'
-    },
-    'Q003': {
-      type: 'YES_NO',
-      text: 'Are you pregnant?'
-    },
     'Q004': {
+      type: 'NUMBER',
+      text: 'Pain scale (0 = no pain, 10 = severe pain)',
+      answer: {
+        min: 0,
+        max: 10
+      }
+    },
+    'Q005': {
+      type: 'YES_NO',
+      text: 'How long have you experienced this headache? (<10 days)'
+    },
+    'Q006': {
       type: 'YES_NO',
       text: 'Do you have a high fever or sign of infections?'
     },
-    'Q005': {
+    'Q007': {
       type: 'YES_NO',
       text: '\
         Do you any of the following: <br> \
@@ -36,43 +46,63 @@ App.factory('HeadacheDiagnosis', function (DiagnosisBuilder) {
     'R001': {
       question: 'Q001',
       answer: {
-        'NUMBER [0, 120]': 'R002'
+        'MALE': 'R003',
+        'FEMALE': 'R002'
       }
     },
     'R002': {
       question: 'Q002',
       answer: {
-        'YES': 'R004',
+        'YES': 'R003',
         'NO': 'R003'
       }
     },
     'R003': {
       question: 'Q003',
       answer: {
-        'YES': 'R004',
-        'NO': 'R004'
+        'AGE [0, 8}': 'R101'
+        'AGE [8, 120]': 'R004'
       }
     },
     'R004': {
       question: 'Q004',
       answer: {
-        'YES': 'R005',
-        'NO': 'R005'
+        'NUMBER [0, 7}': 'R005'
+        'NUMBER [7, 10]': 'R102'
       }
     },
     'R005': {
       question: 'Q005',
       answer: {
-        'YES': 'R100',
-        'NO': 'R101'
+        'YES': 'R006',
+        'NO': 'R102'
+      }
+    },
+    'R006': {
+      question: 'Q006',
+      answer: {
+        'YES': 'R102',
+        'NO': 'R007'
+      }
+    },
+    'R007': {
+      question: 'Q007',
+      answer: {
+        'YES': 'R102',
+        'NO': 'R008'
       }
     },
 
     'R100': {
-      medicines: ['children-advil', 'children-motrin']
+      medicines: ['children-tyenol', 'children-motrin']
     },
+
     'R101': {
-      medicines: ['children-tyenol', 'children-advil', 'children-motrin']
+      message: 'Sorry little cutie pie, you are too new to this world and we scientists do not really understand your body yet. Over-the-counter (OTC) medications are not recommended for your age group. Ask mommy or daddy to take you to your pediatrician.'
+    },
+
+    'R102': {
+      message: 'Over-the-counter (OTC) medications are not approriat for the severity of your current condition. Your condition may be serious and needs to be evaluated by a physician.'
     }
   }
 
